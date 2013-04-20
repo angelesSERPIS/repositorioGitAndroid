23054eb56df4d7c4e41c7d62da278c02b36b08db
angeles <angele.serpis@gmail.com>
2013-04-20 14:06:47 +0200

    comit DesdeUbuntu "para trabajo colaborativo desde dos ordenadores"

--- pruebaEgit/src/com/aulajava/git/TestGit.java
+++ pruebaEgit/src/com/aulajava/git/TestGit.java
@@ -5,5 +5,7 @@ public class TestGit {
    {
 	   System.out.println("HOla a GIT");
 	   System.out.println("HOla a GIT");
+	   System.out.println("Modificacion-1 desde Ubuntu");
+	   System.out.println("Modificacion-2 desde Ubuntu");
    }
 }
