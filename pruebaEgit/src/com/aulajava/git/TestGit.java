b6ef62fba88282c90b9939f2835e9cfea1413091
angeles <angele.serpis@gmail.com>
2013-04-20 14:10:46 +0200

    commmit OtroUbuntu " subir a gitHub"

--- pruebaEgit/src/com/aulajava/git/TestGit.java
+++ pruebaEgit/src/com/aulajava/git/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
 	   System.out.println("HOla a GIT");
 	   System.out.println("Modificacion-1 desde Ubuntu");
 	   System.out.println("Modificacion-2 desde Ubuntu");
+	   System.out.println("Subir a GitHub");
    }
 }
